47ba084a8fe313d8b2eeb63f1b423a08620f2445
ChaninL <chaninlawlert@gmail.com>
2026-04-23 15:35:03 +0700

    Initial Commit

---
+++ Mewt/MewtApp.swift
@@ -0,0 +1,17 @@
+//
+//  MewtApp.swift
+//  Mewt
+//
+//  Created by Ninja on 23/4/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct MewtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
